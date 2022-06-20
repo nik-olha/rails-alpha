207487b54d6bffa9dfb196c5255d0bbc9402c11e
niolha <olhanikulina2@gmail.com>
2022-06-20 14:42:50 +0200

    add creation form

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -4,4 +4,22 @@ class ArticlesController < ApplicationController
     @article = Article.find(params[:id])
   end
 
-end
+  def index 
+    @articles = Article.all
+  end
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new(params.require(:article).permit(:title, :description))
+    if @article.save
+      flash[:notice] = "Article was created successfully."
+      redirect_to @article
+    else
+      render 'new'
+    end
+  end
+
+end 
